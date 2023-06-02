fac308fcb4ca77339f95e0bc525bac568bfb8189
Zackaria Antar <antarzack@gmail.com>
2023-06-02 16:51:08 -0500

    updated router to route requests to the database.

--- server/routes/gallery.router.js
+++ server/routes/gallery.router.js
@@ -1,24 +1,34 @@
 const express = require('express');
 const router = express.Router();
-const galleryItems = require('../modules/gallery.data');
+// const galleryItems = require('../modules/gallery.data');
+const pool = require('../modules/pool')
 
 // DO NOT MODIFY THIS FILE FOR BASE MODE
 
 // PUT Route
 router.put('/like/:id', (req, res) => {
-    console.log(req.params);
-    const galleryId = req.params.id;
-    for(const galleryItem of galleryItems) {
-        if(galleryItem.id == galleryId) {
-            galleryItem.likes += 1;
-        }
-    }
-    res.sendStatus(200);
+    const idToEdit = req.params.id
+    const editQuery = `UPDATE gallery SET likes = likes + 1 WHERE id=$1;`
+
+    pool.query(editQuery, [idToEdit])
+}).then((response)=>{
+    res.sendStatus(200)
+}).catch((error)=>{
+    console.log('Error with the put route', error);
+
+
 }); // END PUT Route
 
 // GET Route
 router.get('/', (req, res) => {
-    res.send(galleryItems);
+    const getQuery = `SELECT * FROM gallery ORDER BY "id" ASC;`
+    pool.query(getQuery)
+}).then((response)=>{
+    console.log('Successfully got data');
+    res.send(response.rows)
+}).catch((err)=>{
+    console.log('Error with the get route',error);
+    response.sendStatus(500)
 }); // END GET Route
 
 module.exports = router;
